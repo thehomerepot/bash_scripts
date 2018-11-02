2232e3b41c9a3be3335c359916b972d6a724135a
thehomerepot <ryan.flagler@gmail.com>
2018-11-02 08:38:54 -0500

    Fixes

--- nv.chart.py
+++ nv.chart.py
@@ -277,7 +277,7 @@ class Service(SimpleService):
 					for memoryLocation in range(5):
 						for eccCounter in range(2):
 							for memError in range(2):
-								_memError[memErrorType[memError]] = pynvml.nvmlDeviceGetMemoryErrorCounter(handle,eccCounter,memError,memoryLocation)
+								_memError[memErrorType[memError]] = pynvml.nvmlDeviceGetMemoryErrorCounter(handle,memError,eccCounter,memoryLocation)
 							_eccCounter[eccCounterType[eccCounter]] = _memError
 						eccErrors[memoryLocationType[memoryLocation]] = _eccCounter
 				except Exception as e:
@@ -310,14 +310,14 @@ class Service(SimpleService):
 
 				## Encoder Utilization
 				try:
-					enc_util = pynvml.nvmlDeviceGetEncoderUtilization(handle)
+					enc_util[encUtil] = pynvml.nvmlDeviceGetEncoderUtilization(handle,encUtil,encSample)
 				except Exception as e:
 					self.debug(str(e))
 					enc_util = None
 
 				## Decoder Utilization
 				try:
-					dec_util = pynvml.nvmlDeviceGetDecoderUtilization(handle)
+					dec_util[decUtil] = pynvml.nvmlDeviceGetDecoderUtilization(handle,decUtil,decSample)
 				except Exception as e:
 					self.debug(str(e))
 					dec_util = None
